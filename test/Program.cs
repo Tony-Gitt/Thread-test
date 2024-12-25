c49c56295eb91acf45f970b2df885f2bec3108f9
Tony <tonytony@123.com>
2024-12-25 15:23:34 +0800

    test commit

---
+++ test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading ;
+var thr =new Thread
+(
+    n =>{
+        for(int i=0;i<10;i++){
+            Console.WriteLine($"In child thread: {n}") ;
+        }
+    Thread.Sleep(1000) ;
+    Thread.Yield() ;
+    }
+);
+thr.Start(999) ;
+for(int i=0;i<10;i++){
+    Console.WriteLine($"In main thread: 888") ;
+}
+thr.Join();
+thr.Join(5000) ;
